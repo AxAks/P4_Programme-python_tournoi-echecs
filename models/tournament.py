cb90b3e48b349f47b64bdb6ef9333d33cc201422
Aksel <axel.akonde@gmail.com>
2021-02-18 01:16:31 +0100

    début tournament class

--- models/tournament.py
+++ models/tournament.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+import re
+
 from datetime import date
 from enum import Enum
 
@@ -10,6 +12,7 @@ class Tournament(Serializable):
     """
     This is the class for the Python Object: Tournament
     """
+    #     Time_control = Enum("Time_control", "BULLET BLITZ RAPIDE")  # vérifier comment ca marche !
     class Time_control(Enum):
         """
         Intermediate class for the tournaments's time control :
@@ -21,11 +24,56 @@ class Tournament(Serializable):
 
     def __init__(self, name: str, location: str, date: date, rounds_count: int,
                  rounds: list, players: list, time_control: Time_control, description: str):
-        self.name = name
-        self.location = location
-        self.date = date
-        self.rounds_count = rounds_count
-        self.rounds = rounds
-        self.players = players
-        self.time_control = time_control
-        self.description = description
+        errors = []
+        try:
+            self.name = name
+        except AttributeError:
+            errors.append('Name')
+        try:
+            self.location = location
+        except AttributeError:
+            errors.append('Location')
+        try:
+            self.date = date
+        except AttributeError:
+            errors.append('Date')
+        try:
+            self.rounds_count = rounds_count
+        except AttributeError:
+            errors.append('Rounds Count')
+        try:
+            self.rounds = rounds
+        except AttributeError:
+            errors.append('Rounds')
+        try:
+            self.players = players
+        except AttributeError:
+            errors.append('Players')
+        try:
+            self.time_control = time_control
+        except AttributeError:
+            errors.append('Time Control')
+        try:
+            self.description = description
+        except AttributeError:
+            errors.append('Description')
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        """
+        Verification of entered characters for last name using regex
+        ASCII table and a few special characters
+        The list of authorized characters is to be completed !
+        :param value:
+        :return:
+        """
+        #  à factoriser ? : doublon avec first_name
+        authorized_characters = re.compile("^[A-ZÉÈÇÀa-zéèçà\ -]+$")
+        if re.match(authorized_characters, value):
+            self.__name = value.title()
+        else:
+            raise AttributeError()
