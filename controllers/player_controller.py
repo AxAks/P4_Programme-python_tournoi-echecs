580949d142ec7ddf9fd55f0f0bf6650e9aa430ef
Aksel <axel.akonde@gmail.com>
2021-03-08 10:00:11 +0100

    tests controller player

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 
 from models.player import Player
+from tinydb import TinyDB
+
 
 """
 Controller file for Player
@@ -16,12 +18,41 @@ plusieurs fichiers controller à écrire : scinder
 
 # player_uuid
 # on donne un uuid, il doit renvoyer un Player
-# tenir un registre des differents players créé
+# tenir un registre des differents players créés
 # doit etre au courant de la création des instances de Player
 # methode factory (voir tuto design pattern)
 
-def get_by_id():
-    pass
+"""
+class Thingy(object):
+    instances = []
+
+    def __init__(self):
+        self.instances.append(self)
+
+def waste_time_and_memory():
+    t = Thingy()
+
+for i in range(5):
+    waste_time_and_memory()
+
+print Thingy.instances
+"""
+class PlayerController:  # ???
+
+    def create_player(self): # à voir !
+        """
+        This method hold a registery of the created players.
+        """
+        pass
+
+    def get_player_by_id(self, player_id):  # ??? à voir !
+        """
+        This method enables to get a Player instance from its identifier attribute.
+        """
+        Player # faux
+        player_id = Player.identifier # faux
+        print(player_id) # faux
+
 
 # on entre un uuid et on recupere un player
 
@@ -31,13 +62,21 @@ def get_by_id():
 # value : instance
 
 
-
-
-
-
-
-
 # unicité
 # pour tournoi
 # propriete getter qui return : name, location, date_pod
-# pour ne pas instancier deux foisplayer_controller.py
+# pour ne pas instancier deux fois player_controller.py
+
+"""
+# Pour sérialiser toutes les instances de joueurs:
+self.instances = [] if self.instances is None else self.instances.append(self) (dans la classe Player)
+
+# Pour sauvegarder plusieurs instances de joueurs serialisées dans TinyDB:
+db = TinyDB(‘db.json’)
+players_table = db.table(‘players’)
+players_table.truncate()	# clear the table first
+players_table.insert_multiple(serialized_players)
+
+# Pour recharger les joueurs sérialisés, tu peux faire ceci
+serialized_players = players_table.all()
+"""
--- tests/tests_controllers/tests_controller_player.py
+++ tests/tests_controllers/tests_controller_player.py
@@ -1,8 +1,27 @@
 # coding=utf-8
 
-from controllers import player_controller
+from controllers.player_controller import PlayerController
+from models.player import Player
+from tests import sample_values as test_sample
+
 """
 File for different tests on the features of the player controller
 """
 
-player_controller.get_by_id()
+
+# instanciation des players
+
+player1 = Player(**test_sample.player1_dict)
+player2 = Player(**test_sample.player2_dict)
+player3 = Player(**test_sample.player3_dict)
+player4 = Player(**test_sample.player4_dict)
+
+
+# tests
+
+print(f'Player1: {player1.identifier}\nPlayer2: {player2.identifier}\n'
+      f'Player3: {player3.identifier}\nPlayer4: {player4.identifier}')
+
+
+for player in Player.instances:
+      print(player.__dict__)
