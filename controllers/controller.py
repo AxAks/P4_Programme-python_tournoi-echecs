174ffa9f6b122bedfadc1518ff2f2718bf1739df
Aksel <axel.akonde@gmail.com>
2021-03-25 10:15:12 +0100

    en chantier, plein de choses à revoir !

---
+++ controllers/controller.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+from views.menus.menu import Menu
+from views.menus.home_menu import HomeMenu
+
+# passer par le controller pour afficher les menus
+# enlever l'intelligence de Menu et la passer dans controller
+# pb mon intelligence pour les menu est dans Menu et le systeme est hérité dans les menus specifique
+# debut avec home_menu et ensuite calquer
+
