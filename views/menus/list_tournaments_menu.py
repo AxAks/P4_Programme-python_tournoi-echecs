d53ce2c795776a9d82eca34cfa8c07ac8a7e7c98
Axel <axel.akonde@cinego.net>
2021-04-08 18:37:38 +0200

    Tournament search by dans menu, à modifier

--- views/menus/list_tournaments_menu.py
+++ views/menus/list_tournaments_menu.py
@@ -13,7 +13,7 @@ class ListTournamentsMenu(Menu):
                          root_page=False, previous_page_ctrl=tournaments_controller.TournamentCtrl,
                          current_page_ctrl=list_tournaments_controller.ListTournamentsCtrl,
                          exiting_message='Now Leaving Chess Tournament Manager')
-        specific_menu_choices = [self.list_all, self.select_one] # à jouter ? sort_by, search_by, etc ... ?
+        specific_menu_choices = [self.list_all, self.search_by_id, self.select_one] # à jouter ? sort_by, search_by, etc ... ?
         [self.choices.append(choice) for choice in specific_menu_choices]
 
     def list_all(self): # list all tournaments
@@ -29,6 +29,10 @@ class ListTournamentsMenu(Menu):
                 print(f'{tournament.name}, {tournament.location}\n '
                       f'{tournament.start_date}, {tournament.end_date}')
         self.current_page_ctrl().run()
+
+    def search_by_id(self):  # à détailler, affiche actuellement l'objet, print les attributs! et verifier le focntionnement !
+        search = input('Search a Tournament by Name, Location or dates : ')
+        print(self.current_page_ctrl().search_by_id(search))
 
     def select_one(self):  #  prints and select one tournament
         print(self.current_page_ctrl().select_one())
