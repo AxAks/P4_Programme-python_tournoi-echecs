a159aaae0ba6603387f2ef4ee9801834e6c4748f
Axel <axel.akonde@cinego.net>
2021-04-09 18:01:06 +0200

    creation fichier add_round et add_match

--- views/inputs/get_properties.py
+++ views/inputs/get_properties.py
@@ -1,23 +1,8 @@
 # coding=utf-8
 
-from datetime import date
-
-from models.models_utils import player_manager
-from views.inputs.generic_inputs import ask_alphabetical_string, ask_alphanumerical_string, ask_iso_date
-
-
-class GetProperties:
+def generate_matchups():
     """
-    Class used in the forms for the input an registering of a new instance.
+    This method randomly generates the tournament match-ups between the Players for the different rounds
+    It takes into account the match-ups that have already been played in the previous rounds.
     """
-
-    # et également ask_identifier ou search_by_id dans player_inputs
-
-
-    #  pas ici, dans un controller !!
-    def generate_matchups(self):
-        """
-        This method randomly generates the tournament match-ups between the Players for the different rounds
-        It takes into account the match-ups that have already been played in the previous rounds.
-        """
-        pass
+    pass
