7d51caf05f0d9bfb4a4cda2d77383da23063bff2
Axel <axel.akonde@cinego.net>
2021-04-20 18:39:56 +0200

    debut redaction algorithme matchmaking dans tournament info contoller

--- controllers/tournament_infos_controller.py
+++ controllers/tournament_infos_controller.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from uuid import UUID
 
 from controllers import list_tournaments_controller
+from controllers.players_controller import PlayerCtrl
 from models.models_utils.player_manager import PlayerManager
 from models.models_utils.supermanager import super_manager as sm
 from models.player import Player
@@ -101,9 +102,25 @@ class TournamentInfosCtrl(Controller):
         This method randomly generates the tournament match-ups between the Players for the different rounds
         It takes into account the match-ups that have already been played in the previous rounds.
         """
-        players = PlayerManager().from_identifier_to_player_obj(self.data.identifiers_list)
-        print(players)
+        sorted_by_ranking = self.sort_tournament_players_by_ranking()
+
+        middle_index = len(sorted_by_ranking) // 2
+        upper_ranking_players_list = sorted_by_ranking[:middle_index]
+        lower_ranking_players_list = sorted_by_ranking[middle_index:]
+
+
+
+
         pass
+
+    def sort_tournament_players_by_ranking(self):
+        tournament_players = []
+        players_identifiers = self.data.identifiers_list
+        for identifier in players_identifiers:
+            player_obj = PlayerManager().from_identifier_to_player_obj(identifier)
+            tournament_players.append(player_obj)
+        sorted_by_ranking = sorted(tournament_players, key=lambda x: x.ranking, reverse=True)
+        return sorted_by_ranking
 
     def add_players_scores(self):
         """
