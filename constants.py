5d4b92881df344496ec419d665cd23bd450e8711
Aksel <axel.akonde@gmail.com>
2021-04-26 22:15:13 +0200

    Ajout attributs Round Results et Tournament total_Results

--- constants.py
+++ constants.py
@@ -20,8 +20,8 @@ ALPHA_NUMERICAL_STRING_RULE = re.compile("^[A-ZÉÈÇÀa-zéèçà0-9_\- ]+$")
 EMPTY_SEARCH_STRINGS = ['', ' ', '-', '_', '.', '()']
 
 TOURNAMENT_PROPERTIES = ['name', 'location', 'start_date', 'end_date', 'rounds', 'identifiers_list',
-                         'time_control', 'description', 'rounds_list']
+                         'time_control', 'description', 'rounds_list', 'total_results']
 
 PLAYER_PROPERTIES = ['last_name', 'identifier', 'first_name', 'birthdate', 'gender', 'ranking']
-ROUND_PROPERTIES = ['name', 'matches', 'start_time', 'end_time']
+ROUND_PROPERTIES = ['name', 'matches', 'start_time', 'end_time', 'results']
 MATCH_PROPERTIES = ['player1_id', 'player1_score', 'player2_id', 'player2_score']
